783a2d61e98273b6a59aeca3be3edb20c7f70a6e
Valdas-Trakumas <valdas.trakumas@ibm.com>
2020-03-10 17:37:40 +0200

    separate functions to work with files

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ namespace MyApp
                     Printer.DisplayWithMedian(studentsList);
                     return true;
                 case "f":
-                    insertDataFromFile(studentsList);
+                    WorkWithFile.insertDataFromFile(studentsList);
                     return true;
                 case "l":
                     Printer.DisplayAverageAndMedian(studentsList);
@@ -143,60 +143,6 @@ namespace MyApp
                 Console.WriteLine("Skaicius gali buti sveikasis arba double tipo");
                 insertFinalMark();
                 return 0;
-            }
-        }
-
-        private static void insertDataFromFile(List<Students> studentsList)
-        {
-            Console.Clear();
-            double grade = 0.0;
-            try
-            {
-                foreach (string line in File.ReadLines("./kursiokai.txt").Skip(1))
-                {
-                    var elements = line.Split(' ').ToArray();
-                    var homeWorkMarks = new double[(elements.Length) - 3];
-                    var finalMark = 0.0;
-                    var counter = 0;
-                    foreach (var it in elements.Select((x, i) => new { Value = x, Index = i }))
-                    {
-                        if (it.Index != 0 && it.Index != 1 && it.Index != elements.Length - 1)
-                        {
-                            if (double.TryParse(it.Value, out grade))
-                            {
-                                homeWorkMarks[counter] = grade;
-                                counter++;
-                            }
-                            else
-                            {
-                                homeWorkMarks[counter] = grade;
-                                counter++;
-                            }
-                        }
-                        if (it.Index == elements.Length - 1)
-                        {
-                            if (double.TryParse(it.Value, out grade))
-                            {
-                                finalMark = grade;
-                            }
-                            else
-                            {
-                                finalMark = grade;
-                            }
-                        }
-                    }
-                    var Student = new Students(elements[0], elements[1]);
-                    Student.SetStudentGrades(homeWorkMarks.ToList());
-                    Student.FinalGrade = finalMark;
-                    studentsList.Add(Student);
-                }
-                Printer.DisplayAverageAndMedian(studentsList);
-
-            }
-            catch (System.Exception)
-            {
-                Console.WriteLine("Blogas arba neegzistuojantis failas!");
-                Console.WriteLine();
             }
         }
     }
